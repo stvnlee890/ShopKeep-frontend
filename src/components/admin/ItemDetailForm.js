bb3770a44d7652d4ee963275459d3b1452f801fb
stvnlee890 <stvnlee890@gmail.com>
2022-08-27 00:40:41 -0700

    working on Item Detail Form styling

--- src/components/admin/ItemDetailForm.js
+++ src/components/admin/ItemDetailForm.js
@@ -1,3 +1,4 @@
+import './styling/itemDetailForm.css'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 import { useParams, useNavigate, Link, Outlet} from 'react-router-dom'
@@ -39,9 +40,6 @@ const ItemDetailForm = ({ username, token, id }) => {
           })
   }
 
-
-
-
   // if(!storeFront){
   //   console.log('loading')
   // } else {
@@ -49,20 +47,67 @@ const ItemDetailForm = ({ username, token, id }) => {
   // }
 
   return (
-    <div>
+    <div className='item-detail-form-container'>
       <h1>Item Detail Form</h1>
-      {/* IMAGE FORM */}
+      <form onSubmit={handleSubmit} className='item-detail-form'>
+      
+        <label className='label'>Store Name</label>
+        <input className='input'
+          onChange={handleChange}
+          placeholder="store front name" 
+          type='text' 
+          id='storeName' 
+          value={storeFront.storeName} />
   
-      {/* STORE FRONT FORM */}
-      <form onSubmit={handleSubmit}>
-        <input onChange={handleChange} placeholder="store front name" type='text' id='storeName' value={storeFront.storeName} />
-        <input onChange={handleChange} placeholder="price" type='text' id='price' value={storeFront.price} />
-        <input onChange={handleChange} placeholder="condition" type='text' id='condition' value={storeFront.condition}/>
-        <input onChange={handleChange} placeholder="color" type='text' id='color' value={storeFront.color}/>
-        <input onChange={handleChange} placeholder="description" type='text' id='description' value={storeFront.description}/>
-        <input onChange={handleChange} placeholder="category" type='text' id='category' value={storeFront.category}/>
-        <button type="submit">Upload</button>
-        {/* <button type='submit'><Link to={`/${username}/setstore/upload-images`}>Upload</Link></button> */}
+
+        <label className='label'>Price</label>
+        <input className='input'
+          onChange={handleChange} 
+          placeholder="price" 
+          type='text' 
+          id='price' 
+          value={storeFront.price} />
+
+        <label className='label'>Size</label>
+        <input className='input'
+          onChange={handleChange} 
+          placeholder="size" 
+          type='text' 
+          id='size' 
+          value={storeFront.size} />
+
+        <label className='label'>Condition</label>
+        <input className='input'
+          onChange={handleChange} 
+          placeholder="condition" 
+          type='text' 
+          id='condition' 
+          value={storeFront.condition}/>
+
+        <label className='label'>Color</label>    
+        <input className='input'
+          onChange={handleChange} 
+          placeholder="color" 
+          type='text' 
+          id='color' 
+          value={storeFront.color}/>
+
+        <label className='label'>Description</label> 
+        <input className='input'
+          onChange={handleChange} 
+          placeholder="description" 
+          type='text' 
+          id='description' 
+          value={storeFront.description}/>
+
+        <label className='label'>Category</label>
+        <input className='input'
+          onChange={handleChange} 
+          placeholder="category" 
+          type='text' 
+          id='category' 
+          value={storeFront.category}/>
+        <button type="submit">Next</button>
       </form>
       <Outlet />
     </div>
