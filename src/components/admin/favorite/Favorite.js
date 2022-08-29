d973c363c22a1839279a788a9ed49535422963a5
stvnlee890 <stvnlee890@gmail.com>
2022-08-29 11:51:57 -0700

    able to delete favorite items

--- src/components/admin/favorite/Favorite.js
+++ src/components/admin/favorite/Favorite.js
@@ -4,7 +4,6 @@ import { useParams, useNavigate } from 'react-router-dom'
 
 const Favorite = () => {
 const { username } = useParams()
-const [storeid, setStoreid] = useState([])
 const [image, setImage] = useState([])
 const navigate = useNavigate()
 
@@ -17,22 +16,41 @@ console.log(image)
 const handleClick = (event) => {
   navigate(`/${event.target.id}`)
 }
+const handleDelete = (event) => {
+    event.preventDefault()
+    console.log(event.target.id)
+    axios.delete(`http://localhost:8080/favorite/${event.target.id}`)
+      .then(() => {
+        const newImage = image.filter(image => image._id !== event.target.id)
+        setImage(newImage)  
+      })
+      .catch((err) => console.log(err))
+  }
+
   
   return (
     <div>
       <h3>
         favorite
       </h3>
+
       {image.map((image) => (
         !image ? <p>loading</p> : 
+        <div>
+
         <img 
         onClick={handleClick}
         key={image._id} 
         alt={'images'}
         id={image.storeFront} 
         src={image.imageUrl} />
+        <button
+        id={image._id}
+        onClick={handleDelete}
+        >delete</button>
+      </div>
       ))}
-       {/* {!image ? <p>loading</p> : <img src={image.imageUrl} />} */}
+     
     </div>
   )
 }
